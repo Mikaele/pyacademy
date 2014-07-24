adb5de7dea465980c93e91448188b31d6321a185
mikaele <mikaele164@gmail.com>
2014-07-24 13:42:37 -0300

    [NEW COMMIT][FISRST]

---
+++ academy/pessoa.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+class Pessoa(object):
+	"""docstring for Pessoa"""
+	def __init__(self, nome, nascimento, ocupacao):
+		self.nome = nome
+		self.nascimento = nascimento
+		self.ocupacao = ocupacao
+
